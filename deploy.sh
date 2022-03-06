bcc6841d8d1e785542e03b7396bf6000b3e75bc1
Hetorus NL <timkleinnijenhuis@hotmail.com>
2022-03-06 02:21:51 +0100

    added zabbix and updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 set -e
 
-echo "making sure server root mount point exists"
-echo "sudo mkdir /mnt/r"
-sudo mkdir -p /mnt/r
-
-echo ""
-echo "make sure that server root is mounted"
-echo "sudo mount -t drvfs R: /mnt/r"
-sudo mount -t drvfs R: /mnt/r
+echo "starting deployment of HetorusNL Status Page"
 
 echo ""
 echo "removing existing content from folder"
-echo "sudo rm -r /mnt/r/status.hetorus.nl/*"
-sudo rm -rf /mnt/r/status.hetorus.nl/*
+echo "sudo rm -r /data/caddy-data/status.hetorus.nl/*"
+sudo rm -rf /data/caddy-data/status.hetorus.nl/*
 
 echo ""
-echo "copying HetorusNL Status Page to the server root"
-echo "cp -r build/* /mnt/r/status.hetorus.nl/"
-cp -r build/* /mnt/r/status.hetorus.nl/
+echo "copying build HetorusNL Status Page to the server root"
+echo "cp -r build/* /data/caddy-data/status.hetorus.nl/"
+cp -r build/* /data/caddy-data/status.hetorus.nl/
 
 echo ""
 echo "finished deployment of HetorusNL Status Page!"
