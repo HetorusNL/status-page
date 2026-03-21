6162d6fd5ee6e5b77447387ad5308f051843e349
HetorusNL <tim@hetorus.nl>
2026-03-21 22:19:42 +0100

    updated deploy script to use rsync instead of rm and copy of local filesystem

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-set -e
+
+# magic options for bash to make scripts safer
+set -Eeuxo pipefail
 
 echo "starting deployment of HetorusNL Status Page"
 
+# modify me for your own setup!
+# be sure to not ruin your filesystem with rsync nuking it all :)
 echo ""
-echo "removing existing content from folder"
-echo "sudo rm -r /data/caddy-data/status.hetorus.nl/*"
-sudo rm -rf /data/caddy-data/status.hetorus.nl/*
-
-echo ""
-echo "copying build HetorusNL Status Page to the server root"
-echo "cp -r build/* /data/caddy-data/status.hetorus.nl/"
-cp -r build/* /data/caddy-data/status.hetorus.nl/
+echo "forcefully copying the built files to the destination"
+rsync -avz --stats --delete --rsync-path="sudo rsync" build/ raptor:/docker/volumes/caddy/srv/status.hetorus.nl/
 
 echo ""
 echo "finished deployment of HetorusNL Status Page!"
